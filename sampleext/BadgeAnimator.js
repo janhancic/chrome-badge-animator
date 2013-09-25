d18d715288e1ebf022fa9000f83abedab5bddcf4
Jan Hančič <jan.hancic@gmail.com>
2013-09-25 22:59:52 +0100

    Update sampleext

--- sampleext/BadgeAnimator.js
+++ sampleext/BadgeAnimator.js
@@ -1,3 +1,14 @@
+/**
+ * @class
+ * @constructor
+ * A Chrome badge (extension button) text animator class.
+ * 
+ * @param {Object} options Configuration options. Available 
+ *  options are: text (the text to display), interval (default: 
+ *  500, how fast to animate), repeat (default: true, should the 
+ *  text start animating from the beginning when animation 
+ *  finishes), size (default: 6, the size of the badge text).
+ */
 function BadgeTextAnimator ( options ) {
 	if ( options == null ) {
 		throw new Error( 'You must pass options to the BadgeTextAnimator' );
@@ -14,7 +25,35 @@ function BadgeTextAnimator ( options ) {
 	this._currentIndex = 0;
 }
 
-BadgeTextAnimator.prototype.doAnimate = function () {
+/**
+ * Start the animation of the badge text.
+ */
+BadgeTextAnimator.prototype.animate = function () {
+	var spaces = [ '', ' ', '  ', '   ', '    ', '     ', '      ' ];
+	this._setBadgeText( spaces[this._options.size] );
+
+	this._doAnimate();
+
+	this._intervalId = setInterval(
+		function () {
+			this._doAnimate();
+		}.bind( this ),
+		this._options.interval
+	);
+};
+
+/**
+ * Stops the animation.
+ */
+BadgeTextAnimator.prototype.stop = function () {
+	clearInterval( this._intervalId );
+	this._intervalId = null;
+
+	this._setBadgeText( '' );
+};
+
+/** @private */
+BadgeTextAnimator.prototype._doAnimate = function () {
 	var startAt = this._currentIndex,
 		cutAt = this._options.size,
 		addBefore = false,
@@ -39,35 +78,19 @@ BadgeTextAnimator.prototype.doAnimate = function () {
 		}
 	}
 
-	chrome.browserAction.setBadgeText( { text: chunk } );
+	this._setBadgeText( chunk );
 
 	this._currentIndex = this._currentIndex + 1;
 	if ( this._currentIndex === this._options.text.length ) {
 		if ( this._options.repeat === true ) {
 			this._currentIndex = 0;
 		} else {
-			this.stop ();
+			this.stop();
 		}
 	}
 };
 
-BadgeTextAnimator.prototype.animate = function () {
-	var spaces = Array ( '', ' ', '  ', '   ', '    ', '     ', '      ' );
-	chrome.browserAction.setBadgeText( { text: spaces[this._options.size] } );
-
-	this.doAnimate();
-
-	this._intervalId = setInterval(
-		function () {
-			this.doAnimate();
-		}.bind( this ),
-		this._options.interval
-	);
-};
-
-BadgeTextAnimator.prototype.stop = function () {
-	clearInterval( this._intervalId );
-	this._intervalId = null;
-
-	chrome.browserAction.setBadgeText( { text: '' } );
+/** @private */
+BadgeTextAnimator.prototype._setBadgeText = function ( text ) {
+	chrome.browserAction.setBadgeText( { text: text } );
 };
--- sampleext/popup.js
+++ sampleext/popup.js
@@ -1,12 +1,10 @@
 window.onload = function() {
-	document.getElementById( 'btnAnimate' ).onclick = function () {
-		var animator = new BadgeTextAnimator ( {
-			text: 'this is some sample text', // text to be scrolled (or animated)
-			interval: 200, // the "speed" of the scrolling
-			repeat: false, // repeat the animation or not
-			size: 6 // size of the badge
-		} );
+	var animator = new BadgeTextAnimator ( {
+		text: 'this is some sample text', // text to be scrolled (or animated)
+		interval: 200, // the "speed" of the scrolling
+		repeat: false, // repeat the animation or not
+		size: 6 // size of the badge
+	} );
 
-		animator.animate ();
-	};
+	animator.animate ();
 };
