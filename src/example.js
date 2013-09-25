325cce520fa76456bb45f0724f12000483c5ac4d
Jan Hančič <jan.hancic@gmail.com>
2013-09-25 22:57:12 +0100

    Style of the example

--- src/example.js
+++ src/example.js
@@ -1,8 +1,8 @@
-var animator = new BadgeTextAnimator ( {
+var animator = new BadgeTextAnimator( {
 	text: 'this is some sample text', // text to be scrolled (or animated)
 	interval: 200, // the "speed" of the scrolling
 	repeat: false, // repeat the animation or not
 	size: 6 // size of the badge
 } );
 
-animator.animate ();
+animator.animate();
